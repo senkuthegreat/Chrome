c8bdafeb79878e86e49d6a6ff2388eef392d6d98
SenkuTheGreat <rohitdahiya245@gmail.com>
2025-06-20 21:12:26 +0530

    Real Start

--- ai_system/control/__init__.py
+++
@@ -1,1 +0,0 @@
-# PC Control Module
--- ai_system/run_ai.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\rohit\My Code Work\Chrome\ai_system"
-echo Installing dependencies...
-py -m pip install -r requirements.txt
-echo.
-echo Starting AI System...
-py main_ai.py
